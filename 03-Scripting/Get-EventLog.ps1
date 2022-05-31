7c8bc0c701f38eae0cce0a8b18b8bb3a537c42dc
Stefan Ober <StefanO@ppedv.de>
2022-05-31 11:28:31 +0200

    Skript mit Verbose ausgabe

--- 03-Scripting/Get-EventLog.ps1
+++ 03-Scripting/Get-EventLog.ps1
@@ -8,4 +8,8 @@ param(
 [string]$ComputerName = "localhost"
 )
 
+Write-Verbose -Message "Ich werde bloß ausgegeben wenn das Skript mit -verbose aufgerufen wird, um mich verwenden zu können sollte [cmdletBinding]param() verwendet werden"
+Write-Verbose -Message "Vom User wurde folgendes angegeben $EventID | $Newest | $ComputerName"
+
+
 Get-EventLog -LogName Security -ComputerName $ComputerName | Where-Object EventId -eq $EventId | Select-Object -First $Newest
